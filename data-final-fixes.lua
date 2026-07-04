206608eb43c4a2a51316f839767731fab684ad12
Em Karisch <emii.khaos@gmail.com>
2026-07-04 16:17:40 +0200

    feat: earlier and cheaper factorissimo

---
+++ data-final-fixes.lua
@@ -0,0 +1,79 @@
+local khaoslib_recipe = require("__khaoslib__.recipe")
+local khaoslib_technology = require("__khaoslib__.technology")
+
+khaoslib_technology:load("factory-architecture-t1")
+  :clear_prerequisites()
+  :unset("unit")
+  :set({
+    research_trigger = {
+      type = "craft-item",
+      count = 30,
+      item = {
+        name = "stone-brick",
+      },
+    },
+  })
+  :commit()
+
+khaoslib_recipe:load("factory-1")
+  :clear_ingredients()
+  :replace_ingredient("stone", {type = "item", name = "stone-brick", amount = 50})
+  :replace_ingredient("iron-plate", function(ingredient)
+    ingredient.amount = 50
+    return ingredient
+  end)
+  :replace_ingredient("copper-plate", function(ingredient)
+    ingredient.amount = 20
+    return ingredient
+  end)
+  :commit()
+
+khaoslib_technology:load("factory-connection-type-fluid")
+  :set({unit = {count = 20}})
+  :commit()
+
+khaoslib_technology:load("factory-interior-upgrade-display")
+  :replace_prerequisite("factory-architecture-t2", "factory-architecture-t1")
+  :set({unit = {count = 50}})
+  :remove_science_pack("logistic-science-pack")
+  :commit()
+
+khaoslib_recipe:load("factory-2")
+  :replace_ingredient("stone-brick", function(ingredient)
+    ingredient.amount = 500
+    return ingredient
+  end)
+  :replace_ingredient("big-electric-pole", function(ingredient)
+    ingredient.amount = 10
+    return ingredient
+  end)
+  :commit()
+
+khaoslib_technology:load("factory-architecture-t3")
+  :remove_prerequisite("production-science-pack")
+  :remove_science_pack("production-science-pack")
+  :set({unit = {count = 1000}})
+  :commit()
+
+khaoslib_recipe:load("factory-3")
+  :replace_ingredient("steel-plate", function(ingredient)
+    ingredient.amount = 500
+    return ingredient
+  end)
+  :replace_ingredient("substation", function(ingredient)
+    ingredient.amount = 10
+    return ingredient
+  end)
+  :replace_ingredient("concrete", function(ingredient)
+    ingredient.amount = 1000
+    return ingredient
+  end)
+  :commit()
+
+if mods["space-age"] then
+  khaoslib_technology:load("factory-upgrade-greenhouse")
+    :add_prerequisite("agricultural-science-pack")
+    :add_science_pack({"space-science-pack", 1})
+    :add_science_pack({"agricultural-science-pack", 1})
+    :commit()
+end
---
+++ settings-final-fixes.lua
@@ -0,0 +1,5 @@
+local Settings = require("__cf-lib__.settings.Settings")
+
+if mods["space-age"] then
+  Settings.force("Factorissimo2-cheap-research", true)
+end
